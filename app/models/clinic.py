24afeac2adb9f49c75820c83b1a9e1ec6b95efca
Pavel Bobkov <83582393+tniur@users.noreply.github.com>
2025-04-08 20:22:09 +0300

    delete unnecessary models

--- app/models/clinic.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class Clinic(BaseModel):
-    id: int
-    title: str
-    doctor_name: str
-    real_address: str
-    phone: str
--- app/models/profession.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class Profession(BaseModel):
-    id: int
-    name: str
-    doctor_name: str
-    is_deleted: bool
