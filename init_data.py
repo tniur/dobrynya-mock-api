4509f93e811ac5adcb6b85778cfdeaa29dbffdb2
Pavel Bobkov <83582393+tniur@users.noreply.github.com>
2025-04-10 03:38:56 +0300

    project management

--- init_data.py
+++ init_data.py
@@ -1,6 +1,7 @@
 import json
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from pathlib import Path
 from app.db.models import (
     Base, Clinic, Profession, User, Patient, PatientKey, PatientLabResult,
     Service, ServiceCategories, user_clinics, user_professions, user_services,
@@ -15,17 +16,30 @@ def load_data():
     Base.metadata.create_all(bind=engine)
     db = SessionLocal()
 
-    with open("../data/clinics.json") as f:
+    clinics_path = Path(__file__).parent / 'data' / 'clinics.json'
+    professions_path = Path(__file__).parent / 'data' / 'professions.json'
+    users_path = Path(__file__).parent / 'data' / 'users.json'
+    service_categories_path = Path(__file__).parent / 'data' / 'service_categories.json'
+    services_path = Path(__file__).parent / 'data' / 'services.json'
+    patients_path = Path(__file__).parent / 'data' / 'patients.json'
+    patient_keys_path = Path(__file__).parent / 'data' / 'patient_keys.json'
+    lab_results_path = Path(__file__).parent / 'data' / 'lab_results.json'
+    patient_lab_result_details_path = Path(__file__).parent / 'data' / 'patient_lab_result_details.json'
+    patient_documents_path = Path(__file__).parent / 'data' / 'patient_documents.json'
+    patient_consultations_path = Path(__file__).parent / 'data' / 'patient_consultations.json'
+    patient_appointments_path = Path(__file__).parent / 'data' / 'patient_appointments.json'
+
+    with open(clinics_path) as f:
         clinics = json.load(f)
         for item in clinics:
             db.add(Clinic(**item))
 
-    with open("../data/professions.json") as f:
+    with open(professions_path) as f:
         professions = json.load(f)
         for item in professions:
             db.add(Profession(**item))
 
-    with open("../data/users.json") as f:
+    with open(users_path) as f:
         users = json.load(f)
         for item in users:
             professions = item.pop("profession", [])
@@ -43,12 +57,12 @@ def load_data():
             for sid in services:
                 db.execute(user_services.insert().values(user_id=user.id, service_id=sid))
 
-    with open("../data/service_categories.json") as f:
+    with open(service_categories_path) as f:
         service_categories = json.load(f)
         for item in service_categories:
             db.add(ServiceCategories(**item))
 
-    with open("../data/services.json") as f:
+    with open(services_path) as f:
         services = json.load(f)
         for item in services:
             categories = item.pop("category_id", [])
@@ -60,37 +74,37 @@ def load_data():
             for cid in categories:
                 db.execute(service_service_categories.insert().values(service_id=service.id, category_id=cid))
 
-    with open("../data/patients.json") as f:
+    with open(patients_path) as f:
         patients = json.load(f)
         for item in patients:
             db.add(Patient(**item))
 
-    with open("../data/patient_keys.json") as f:
+    with open(patient_keys_path) as f:
         keys = json.load(f)
         for item in keys:
             db.add(PatientKey(**item))
 
-    with open("../data/lab_results.json") as f:
+    with open(lab_results_path) as f:
         lab_results = json.load(f)
         for item in lab_results:
             db.add(PatientLabResult(**item))
 
-    with open("../data/patient_lab_result_details.json") as f:
+    with open(patient_lab_result_details_path) as f:
         details = json.load(f)
         for item in details:
             db.add(PatientLabResultDetail(**item))
 
-    with open("../data/patient_documents.json") as f:
+    with open(patient_documents_path) as f:
         documents = json.load(f)
         for item in documents:
             db.add(PatientDocument(**item))
 
-    with open("../data/patient_consultations.json") as f:
+    with open(patient_consultations_path) as f:
         consultations = json.load(f)
         for item in consultations:
             db.add(PatientConsultation(**item))
 
-    with open("../data/patient_appointments.json") as f:
+    with open(patient_appointments_path) as f:
         appointments = json.load(f)
         for item in appointments:
             db.add(PatientAppointment(**item))
