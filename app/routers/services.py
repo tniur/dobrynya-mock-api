473b56025d6b2dcbcb3c9f5569398afce4b4c7c2
Pavel Bobkov <pavel.bbkv1@gmail.com>
2025-05-14 18:11:30 +0300

    add profession title for services body

--- app/routers/services.py
+++ app/routers/services.py
@@ -3,6 +3,7 @@ from typing import Optional
 from sqlalchemy.orm import Session
 from app.db.database import get_db
 from app.db.crud import get_filtered_services
+from app.db.models import Profession
 
 router = APIRouter()
 
@@ -25,12 +26,16 @@ def get_services(
 
     result = []
     for service in services:
+        profession = db.query(Profession).filter_by(id=service.profession_id).first()
+        profession_title = profession.doctor_name.lower() if profession and profession.doctor_name else None
+
         result.append({
             "id": service.id,
             "title": service.title,
             "price": service.price,
             "duration": service.duration,
             "profession_id": service.profession_id,
+            "profession_title": profession_title,
             "category_id": [c.id for c in service.category_ids],
             "short_desc": service.short_desc,
             "full_desc": service.full_desc,
